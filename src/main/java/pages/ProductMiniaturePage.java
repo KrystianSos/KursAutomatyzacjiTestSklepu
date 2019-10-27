10fa4f692c72fd0a8af649778e801963c54eccf1
ala makota <kw.dziubinska@gmail.com>
2019-10-27 11:48:46 +0100

    FindBy in ProductMiniaturePage

--- src/main/java/pages/ProductMiniaturePage.java
+++ src/main/java/pages/ProductMiniaturePage.java
@@ -2,6 +2,7 @@ package pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory;
 
@@ -10,4 +11,23 @@ public class ProductMiniaturePage extends BasePage {
         super(driver);
         PageFactory.initElements(new DefaultElementLocatorFactory(element), this);
     }
+
+    @FindBy(className = "price")
+    private WebElement price;
+
+    @FindBy(className = "regular-price")
+    private WebElement priceBeforeDiscount;
+
+    @FindBy(className = "h3")
+    private WebElement name;
+
+    @FindBy(className = "new")
+    private WebElement newLabel;
+
+    @FindBy(css = "div > a > img")
+    private WebElement image;
+
+    @FindBy(className = "discount-percentage")
+    private WebElement discountLabel;
+
 }
